32e354c11250c077704c703eae035214deea80d5
MattiaBenzoni <mattiabenzoni@gmail.com>
2015-01-08 19:35:06 +0100

    Game_board Test aggiunta

---
+++ app/src/androidTest/java/com/project/mobile_application/sixbowls/Model/Game_boardTest.java
@@ -0,0 +1,11 @@
+package com.project.mobile_application.sixbowls.Model;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by MattiaBenzoni on 08/01/2015.
+ */
+public class Game_boardTest extends TestCase {
+
+    Game_board board= new Game_board();
+}
