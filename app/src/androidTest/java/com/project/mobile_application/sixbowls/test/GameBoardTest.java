f7a1e0cf3218c41cb20c082232ec449eff43fa75
martino <m.t.n@hotmail.it>
2015-01-09 21:05:21 +0100

    git issue

---
+++ app/src/androidTest/java/com/project/mobile_application/sixbowls/test/GameBoardTest.java
@@ -0,0 +1,27 @@
+package com.project.mobile_application.sixbowls.test;
+
+import com.project.mobile_application.sixbowls.Model.GameBoard;
+
+import junit.framework.TestCase;
+
+
+/**
+ * Created by MattiaBenzoni on 08/01/2015.
+ */
+
+public class GameBoardTest extends TestCase {
+
+    GameBoard board;
+
+    private void testFromStringConfiguration(char[] inputConfiguration) {
+
+    }
+
+    public void testSeedingPhase() throws Exception {
+
+    }
+
+    public void testCheckGameOver() throws Exception {
+
+    }
+}
