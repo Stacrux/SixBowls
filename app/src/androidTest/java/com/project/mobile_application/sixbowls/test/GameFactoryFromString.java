f0b0100f0d5ba151c055f28b18633d98b379576c
martino <m.t.n@hotmail.it>
2015-01-10 15:34:47 +0100

    testing, added a factory class for building custom gameboards

---
+++ app/src/androidTest/java/com/project/mobile_application/sixbowls/test/GameFactoryFromString.java
@@ -0,0 +1,22 @@
+package com.project.mobile_application.sixbowls.test;
+
+import com.project.mobile_application.sixbowls.Model.GameBoard;
+
+/**
+ * Created by Martino on 10/01/2015.
+ */
+public class GameFactoryFromString {
+
+    GameBoard gameboard = null;
+
+    /**
+     *
+     * @param configuration : a string of type YBXBXBXBXBXBXTX
+     */
+    public GameBoard generateBoard(String configuration){
+        return gameboard;
+    }
+
+
+
+}
