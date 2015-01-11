d9e26b5649a7a71ffec11457e4525eeb9faa0262
martino <m.t.n@hotmail.it>
2015-01-11 19:05:14 +0100

    activities schema

---
+++ app/src/main/java/com/project/mobile_application/sixbowls/InstructionsActivity.java
@@ -0,0 +1,9 @@
+package com.project.mobile_application.sixbowls;
+
+import android.app.Activity;
+
+/**
+ * Created by Martino on 11/01/2015.
+ */
+public class InstructionsActivity extends Activity {
+}
