a9468737afc2f17c3f87300b98191754e4613f09
MattiaBenzoni <mattiabenzoni@gmail.com>
2015-01-11 12:06:26 +0100

    Test metodo inner seeding

--- app/src/androidTest/java/com/project/mobile_application/sixbowls/test/PersonalSetTest.java
+++ app/src/androidTest/java/com/project/mobile_application/sixbowls/test/PersonalSetTest.java
@@ -1,9 +1,13 @@
 package com.project.mobile_application.sixbowls.test;
 
 import com.project.mobile_application.sixbowls.Model.Bowl;
+import com.project.mobile_application.sixbowls.Model.Constants;
 import com.project.mobile_application.sixbowls.Model.PersonalSet;
 import com.project.mobile_application.sixbowls.Model.Player;
+import com.project.mobile_application.sixbowls.Model.StandardGame.BowlStandard;
 import com.project.mobile_application.sixbowls.Model.StandardGame.PersonalSetStandard;
+import com.project.mobile_application.sixbowls.Model.StandardGame.TrayStandard;
+import com.project.mobile_application.sixbowls.Model.Tray;
 
 import junit.framework.TestCase;
 
@@ -14,9 +18,11 @@ import java.util.ArrayList;
  */
 public class PersonalSetTest extends TestCase {
 
-    PersonalSet personalSet;
-    Player player;
-    ArrayList<Bowl>bowls=new ArrayList<Bowl>();
+    private PersonalSet personalSet;
+    private Player player;
+    private ArrayList<Bowl>bowls=new ArrayList<Bowl>();
+    private Tray tray;
+
 
 
     @Override
@@ -26,9 +32,29 @@ public class PersonalSetTest extends TestCase {
 
     /**
      * The first test for innerSeeding method
+     * The bowlId=3 and the number of seeds=10
      */
     public void innerSeedingTest(){
-        personalSet = initialization();
+
+        // 3 3 10 3 3 3   0
+        // 3 3 0  4 4 4   1          6
+
+        personalSet = initialization(true);
+        int remainder=personalSet.innerSeeding(2,10);
+
+        assertEquals(remainder,6);
+        assertEquals(personalSet.toString(),"1B3B3B0B4B4B4T1");
+    }
+
+    public void innerSeedingTest1(){
+
+
+
+        personalSet = initialization(false);
+        int remainder=personalSet.innerSeeding(2,2);
+
+        assertEquals(remainder,0);
+        assertEquals(personalSet.toString(),"0B4B4B3B3B3B3T0");
     }
 
     @Override
@@ -36,9 +62,17 @@ public class PersonalSetTest extends TestCase {
 
     }
 
-    private PersonalSetStandard initialization(){
-       player=new Player(0);
+    private PersonalSetStandard initialization(boolean state){
 
-       return null;
+       PersonalSetStandard personal;
+       player=new Player(0);
+       for (int e = 0; e < Constants.numberOfBowls; e++) {
+            BowlStandard bowl = new BowlStandard(Constants.initialBowlContent, e);
+            bowls.add(bowl);
+       }
+       tray=new TrayStandard(Constants.initialTrayContent);
+       personal=new PersonalSetStandard(player,bowls,tray,state);
+
+       return personal;
     }
 }
