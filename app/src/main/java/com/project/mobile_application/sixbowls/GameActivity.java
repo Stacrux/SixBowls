b635ef5cafab54a9deeeaf89e958770595daac13
martino <m.t.n@hotmail.it>
2015-01-12 16:40:16 +0100

    drawable try for game activity

--- app/src/main/java/com/project/mobile_application/sixbowls/GameActivity.java
+++ app/src/main/java/com/project/mobile_application/sixbowls/GameActivity.java
@@ -3,6 +3,7 @@ package com.project.mobile_application.sixbowls;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 
@@ -17,6 +18,12 @@ public class GameActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        //imposta l'orientamento dello schermo
+        //setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        //nascondi statusbar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         String dato1 = getIntent().getExtras().getString("gameType");
 
